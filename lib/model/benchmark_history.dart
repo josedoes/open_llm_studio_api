07cc8ae30529df1ce8776fb0d92e03eb8d5a2f46
joselara <jose@romantechnologies.co.uk>
2023-08-27 01:54:02 -0400

    benchmark variable name update

--- lib/model/benchmark_history.dart
+++ lib/model/benchmark_history.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 
 class BenchmarkHistory extends Equatable {
-  BenchmarkHistory({required this.id, required this.uid, required this.spaceId, required this.prompt, required this.benchmarkPrompt, required this.modelUsed, required this.firestoreDocumentId, required this.validatorRules, required this.totalTrueByHuman, required this.millisecondsRun, required this.tokens, required this.approximatePrice, required this.date, required this.totalQuestions, required this.totalMissed, required this.totalTrue});
+  BenchmarkHistory({required this.id, required this.uid, required this.spaceId, required this.prompt, required this.benchmarkPrompt, required this.modelUsed, required this.cloudStorageUrl, required this.validatorRules, required this.totalTrueByHuman, required this.millisecondsRun, required this.tokens, required this.approximatePrice, required this.date, required this.totalQuestions, required this.totalMissed, required this.totalTrue});
 
   final String? id;
   final String? uid;
@@ -9,7 +9,7 @@ class BenchmarkHistory extends Equatable {
   final String? prompt;
   final String? benchmarkPrompt;
   final String? modelUsed;
-  final String? firestoreDocumentId;
+  final String? cloudStorageUrl;
   final String? validatorRules;
   final int? totalTrueByHuman;
   final int? millisecondsRun;
@@ -21,22 +21,22 @@ class BenchmarkHistory extends Equatable {
   final int? totalTrue;
 
   @override
-  List<Object?> get props => [id, uid, spaceId, prompt, benchmarkPrompt, modelUsed, firestoreDocumentId, validatorRules, totalTrueByHuman, millisecondsRun, tokens, approximatePrice, date, totalQuestions, totalMissed, totalTrue];
+  List<Object?> get props => [id, uid, spaceId, prompt, benchmarkPrompt, modelUsed, cloudStorageUrl, validatorRules, totalTrueByHuman, millisecondsRun, tokens, approximatePrice, date, totalQuestions, totalMissed, totalTrue];
 
   Map<String, dynamic> toJson() {
-    return {'id': id, 'uid': uid, 'spaceId': spaceId, 'prompt': prompt, 'benchmarkPrompt': benchmarkPrompt, 'modelUsed': modelUsed, 'firestoreDocumentId': firestoreDocumentId, 'validatorRules': validatorRules, 'totalTrueByHuman': totalTrueByHuman, 'millisecondsRun': millisecondsRun, 'tokens': tokens, 'approximatePrice': approximatePrice, 'date': date, 'totalQuestions': totalQuestions, 'totalMissed': totalMissed, 'totalTrue': totalTrue};
+    return {'id': id, 'uid': uid, 'spaceId': spaceId, 'prompt': prompt, 'benchmarkPrompt': benchmarkPrompt, 'modelUsed': modelUsed, 'cloudStorageUrl': cloudStorageUrl, 'validatorRules': validatorRules, 'totalTrueByHuman': totalTrueByHuman, 'millisecondsRun': millisecondsRun, 'tokens': tokens, 'approximatePrice': approximatePrice, 'date': date, 'totalQuestions': totalQuestions, 'totalMissed': totalMissed, 'totalTrue': totalTrue};
   }
 
   factory BenchmarkHistory.fromJson(Map<String, dynamic> map) {
-    return BenchmarkHistory(id: map['id'], uid: map['uid'], spaceId: map['spaceId'], prompt: map['prompt'], benchmarkPrompt: map['benchmarkPrompt'], modelUsed: map['modelUsed'], firestoreDocumentId: map['firestoreDocumentId'], validatorRules: map['validatorRules'], totalTrueByHuman: map['totalTrueByHuman'], millisecondsRun: map['millisecondsRun'], tokens: map['tokens'], approximatePrice: map['approximatePrice'], date: map['date'], totalQuestions: map['totalQuestions'], totalMissed: map['totalMissed'], totalTrue: map['totalTrue']);
+    return BenchmarkHistory(id: map['id'], uid: map['uid'], spaceId: map['spaceId'], prompt: map['prompt'], benchmarkPrompt: map['benchmarkPrompt'], modelUsed: map['modelUsed'], cloudStorageUrl: map['cloudStorageUrl'], validatorRules: map['validatorRules'], totalTrueByHuman: map['totalTrueByHuman'], millisecondsRun: map['millisecondsRun'], tokens: map['tokens'], approximatePrice: map['approximatePrice'], date: map['date'], totalQuestions: map['totalQuestions'], totalMissed: map['totalMissed'], totalTrue: map['totalTrue']);
   }
 
-  BenchmarkHistory copyWith({String? id, String? uid, String? spaceId, String? prompt, String? benchmarkPrompt, String? modelUsed, String? firestoreDocumentId, String? validatorRules, int? totalTrueByHuman, int? millisecondsRun, int? tokens, int? approximatePrice, int? date, int? totalQuestions, int? totalMissed, int? totalTrue}) {
-    return BenchmarkHistory(id: id ?? this.id, uid: uid ?? this.uid, spaceId: spaceId ?? this.spaceId, prompt: prompt ?? this.prompt, benchmarkPrompt: benchmarkPrompt ?? this.benchmarkPrompt, modelUsed: modelUsed ?? this.modelUsed, firestoreDocumentId: firestoreDocumentId ?? this.firestoreDocumentId, validatorRules: validatorRules ?? this.validatorRules, totalTrueByHuman: totalTrueByHuman ?? this.totalTrueByHuman, millisecondsRun: millisecondsRun ?? this.millisecondsRun, tokens: tokens ?? this.tokens, approximatePrice: approximatePrice ?? this.approximatePrice, date: date ?? this.date, totalQuestions: totalQuestions ?? this.totalQuestions, totalMissed: totalMissed ?? this.totalMissed, totalTrue: totalTrue ?? this.totalTrue);
+  BenchmarkHistory copyWith({String? id, String? uid, String? spaceId, String? prompt, String? benchmarkPrompt, String? modelUsed, String? cloudStorageUrl, String? validatorRules, int? totalTrueByHuman, int? millisecondsRun, int? tokens, int? approximatePrice, int? date, int? totalQuestions, int? totalMissed, int? totalTrue}) {
+    return BenchmarkHistory(id: id ?? this.id, uid: uid ?? this.uid, spaceId: spaceId ?? this.spaceId, prompt: prompt ?? this.prompt, benchmarkPrompt: benchmarkPrompt ?? this.benchmarkPrompt, modelUsed: modelUsed ?? this.modelUsed, cloudStorageUrl: cloudStorageUrl ?? this.cloudStorageUrl, validatorRules: validatorRules ?? this.validatorRules, totalTrueByHuman: totalTrueByHuman ?? this.totalTrueByHuman, millisecondsRun: millisecondsRun ?? this.millisecondsRun, tokens: tokens ?? this.tokens, approximatePrice: approximatePrice ?? this.approximatePrice, date: date ?? this.date, totalQuestions: totalQuestions ?? this.totalQuestions, totalMissed: totalMissed ?? this.totalMissed, totalTrue: totalTrue ?? this.totalTrue);
   }
 
  static Map<String, dynamic> exampleJson() {
-    return {'id': "", 'uid': "", 'spaceId': "", 'prompt': "", 'benchmarkPrompt': "", 'modelUsed': "", 'firestoreDocumentId': "", 'validatorRules': "", 'totalTrueByHuman': 0, 'millisecondsRun': 0, 'tokens': 0, 'approximatePrice': 0, 'date': 0, 'totalQuestions': 0, 'totalMissed': 0, 'totalTrue': 0};
+    return {'id': "", 'uid': "", 'spaceId': "", 'prompt': "", 'benchmarkPrompt': "", 'modelUsed': "", 'cloudStorageUrl': "", 'validatorRules': "", 'totalTrueByHuman': 0, 'millisecondsRun': 0, 'tokens': 0, 'approximatePrice': 0, 'date': 0, 'totalQuestions': 0, 'totalMissed': 0, 'totalTrue': 0};
   }
 bool match(Map map){
     final model = toJson();
